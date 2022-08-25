613fac65af85af540dff7eb0209dec58f0b95289
Hidxlg0 <justinh8100@gmail.com>
2022-08-24 22:29:50 -0500

    new dist

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,12 @@ import 'firebase/auth';
 import 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: `${process.env.VUE_APP_FIREBASE_API_KEY}`,
-    authDomain: "quizalt-6d3fa.firebaseapp.com",
-    projectId: "quizalt-6d3fa",
-    storageBucket: "quizalt-6d3fa.appspot.com",
-    messagingSenderId: "395917127891",
-    appId: "1:395917127891:web:0a20990167316fc50f6426",
-    measurementId: "G-GBVSQJGZ6W"
+    apiKey: "AIzaSyBblNVwdWX7ZCNYM0fqiZnxnyAqEgqrH74",
+    authDomain: "quizalt-2.firebaseapp.com",
+    projectId: "quizalt-2",
+    storageBucket: "quizalt-2.appspot.com",
+    messagingSenderId: "1034641615935",
+    appId: "1:1034641615935:web:983000035584ce462f1c8a"
 };
 
 firebase.initializeApp(firebaseConfig);
